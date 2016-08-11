6cf4aa8a9d1d044b6a8f202c19d159a1f3400a2c
rgordeev <roma.gordeev@gmail.com>
2016-08-11 19:51:19 +0400

    p16 problem

---
+++ src/test/groovy/challenges/codility/p16/SolutionSpec.groovy
@@ -0,0 +1,121 @@
+package challenges.codility.p16
+
+import spock.lang.Specification
+
+/**
+ * Created by rgordeev on 11.08.16.
+ */
+class SolutionSpec extends Specification {
+
+    def "check number for prime"() {
+        given:
+            def solution = new Solution()
+        expect: solution.isPrime(n) == f
+        where:
+            n       |   f
+            20      |   false
+            1       |   false
+            2       |   true
+            3       |   true
+            4       |   false
+            5       |   true
+            6       |   false
+            7       |   true
+            8       |   false
+            0       |   false
+            9       |   false
+            100     |   false
+            101     |   true
+            1000    |   false
+            1001    |   false
+            11      |   true
+    }
+
+    def "check number to array mapper"() {
+        given:
+            def solution = new Solution()
+        expect:
+            solution.numberToArray(n) == arr
+        where:
+            n       |   arr
+            1       |   [1]
+            2       |   [2]
+            9       |   [9]
+            0       |   [0]
+            10      |   [0, 1]
+            15      |   [5, 1]
+            123     |   [3, 2, 1]
+            154     |   [4, 5, 1]
+            1456731 |   [1, 3, 7, 6, 5, 4, 1]
+    }
+
+    def "check rotate algorithm"() {
+        given:
+            def solution = new Solution()
+        expect:
+            solution.rotate(n, a) == b
+        where:
+            n       |       a                               |       b
+            3       |   [1, 2, 3, 4] as Integer[]           |   [2, 3, 4, 1]
+            4       |   [1, 2, 3, 4] as Integer[]           |   [1, 2, 3, 4]
+            2       |   [1, 2, 3, 4] as Integer[]           |   [3, 4, 1, 2]
+    }
+
+    def "check reverse work well"() {
+        given:
+            def solution = new Solution();
+        expect:
+            solution.reverse(a) == b
+        where:
+            a                       |   b
+            [1] as Integer[]        |   [1]
+            []  as Integer[]        |   []
+            [1,2] as Integer[]      |   [2, 1]
+            [1,2,3,4] as Integer[]  |   [4,3,2,1]
+            [4,3,2] as Integer[]    |   [2,3,4]
+    }
+
+    def "check array to int conversion"() {
+        given:
+            def solution = new Solution()
+        expect:
+            solution.arrayToInt(a) == n
+        where:
+            a                           |   n
+            [1,2,3,4] as Integer[]      |   1234
+            [1] as Integer[]            |   1
+            [1,2] as Integer[]          |   12
+            [1,4,2,1,0] as Integer[]    |   14210
+    }
+
+    def "check rotations generation"() {
+        given:
+            def solution = new Solution()
+        expect:
+            solution.rotations(n) == rotations
+        where:
+            n       |   rotations
+            1       |   [1]
+            0       |   [0]
+            12      |   [12, 21]
+            11      |   [11, 11]
+            123     |   [123, 312, 231]
+            567     |   [567, 756, 675]
+    }
+
+    def "check solution"() {
+        given:
+        def solution = new Solution()
+        expect:
+        solution.solution(n) == m
+        where:
+        m       |   n
+        0       |   1
+        4       |   10
+        13      |   100
+        25      |   1000
+        33      |   10000
+        43      |   100000
+        55      |   1000000
+    }
+}
